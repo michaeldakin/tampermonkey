a90a26b0ab7e66ecfbfbdb33241e82590c747ce9
Michael Dakin <4318690+michaeldakin@users.noreply.github.com>
2024-01-03 14:09:32 +1100

    Init project, add statusHighlight to repo

---
+++ caseHighlight/statusHighligh.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         caseHighlighter.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Highlight cases with specific table paramaters for Salesforce Performance edition
+// @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
+// @author       Michael Dakin
+// @match        https://*.my.salesforce.com/500*
+// @grant        none
+// ==/UserScript==
+
+/*
+* Version history:
+* 0.1 - here we are
+*/
+
+const DEBUG = true;
+
+function log(logmessage) {
+    var logtime = new Date().toISOString();
+    if (DEBUG == true) {
+        console.log("[UserScipt:caseHighlighter.js] " + logtime + " [DEBUG] " + logmessage);
+    }
+}
+
+// Main func to loop over SFDC table nd find cases with desired status and highlight the status to make it much easier to view cases requiring attention at a glance
+function getCaseStatus() {
+    var caseRows = document.getElementsByClassName("x-grid3-row-table");
+    // var caseID = caseRows[row].rows[0].cells[caseNumFieldCol].innerText;
+
+    if (document.querySelector('[title="Case Number"]') != null) {
+        var caseNumFieldCol = document.querySelector('[title="Case Number"]').parentNode.cellIndex; // get the index of the case status
+        log("caseNumFieldCol " + caseNumFieldCol);
+    }
+
+    if (document.querySelector('[title="Status"]') != null) {
+        var statusFieldCol = document.querySelector('[title="Status"]').parentNode.cellIndex; // get the index of the case status
+        log("statusFieldCol " + statusFieldCol);
+    }
+
+    function highlightStatusField(bgColour, textColour) {
+        caseRows[row].rows[0].cells[statusFieldCol].style.background = bgColour;
+        caseRows[row].rows[0].cells[statusFieldCol].style.color = textColour;
+    }
+
+    let totalCases = 0;
+    for (var row = 0; row < caseRows.length; row++) {
+        var statusField = caseRows[row].rows[0].cells[statusFieldCol];
+
+        if (statusField.innerText === "Customer Response Provided") {
+            highlightStatusField("red", "white")
+        }
+
+        totalCases += 1;
+    }
+    log("Total cases: " + totalCases);
+}
+
+waitForKeyElements("div.x-grid3-row.x-grid3-row-last", getCaseStatus);
