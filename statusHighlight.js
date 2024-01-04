43069723c07529d5bbc7a7f43f5c37c3d5330ebf
Michael Dakin <4318690+michaeldakin@users.noreply.github.com>
2024-01-04 11:17:38 +1100

    (feat): added setTimeout and priority highlighting

--- statusHighlight.js
+++ statusHighlight.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         statusHighlight.js
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Highlight cases with specific table paramaters for Salesforce Performance edition
 // @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
 // @author       Michael Dakin
@@ -14,12 +14,11 @@
 * Version history:
 * 0.1 - here we are
 * 0.2 - cleaned up legacy code and using a new version of waitForKeyElements - no jQuery!
+* 0.3 - added priority highlighting and timeout to run the script every WAITTIME seconds (currently 5)
 */
 
-const DEBUG = true;
-
 function log(logmessage) {
-    let logtime = new Date().toISOString();
+    var logtime = new Date().toISOString();
     if (DEBUG == true) {
         console.log("[UserScipt:caseHighlighter.js] " + logtime + " [DEBUG] " + logmessage);
     }
@@ -27,9 +26,20 @@ function log(logmessage) {
 
 // Loop over `caseRows` table and find `Case Number` & `Status` fields, we then apply custom styling
 function getCaseStatus() {
+    var DEBUG = false;
+
+    // Get case table from /500*
+    let caseRows = document.getElementsByClassName("x-grid3-row-table");     // resets
+
+    // Get cell index of rows by column name
     if (document.querySelector('[title="Case Number"]') != null) {
         var caseNumFieldCol = document.querySelector('[title="Case Number"]').parentNode.cellIndex; // get the index of the case status
         log("caseNumFieldCol " + caseNumFieldCol);
+    }
+
+    if (document.querySelector('[title="Priority"]') != null) {
+        var casePriorityCol = document.querySelector('[title="Priority"]').parentNode.cellIndex; // get the index of the Priority column
+        log("casePriorityCol", casePriorityCol);
     }
 
     if (document.querySelector('[title="Status"]') != null) {
@@ -37,20 +47,27 @@ function getCaseStatus() {
         log("statusFieldCol " + statusFieldCol);
     }
 
-    let caseRows = document.getElementsByClassName("x-grid3-row-table");
-    let totalCases = 0;
-    let totalCRP = 0;
-
+    // Loop over `caseRows` and highlight Status field if it equals Customer Response Provided
+    // TODO: move this to a map / array to allow multiple field highlighting
     for (let row = 0; row < caseRows.length; row++) {
-        let statusField = caseRows[row].rows[0].cells[statusFieldCol];
-        let caseID = caseRows[row].rows[0].cells[caseNumFieldCol].innerText;
+        if (caseNumFieldCol != null) { var caseID = caseRows[row].rows[0].cells[caseNumFieldCol].innerText; log(caseID, "caseID: " + caseNumFieldCol.innerText); }
+        if (statusFieldCol != null) { var statusField = caseRows[row].rows[0].cells[statusFieldCol]; log(caseID, "statusField: " + statusField.innerText); }
+        if (casePriorityCol != null) { var casePriority = caseRows[row].rows[0].cells[casePriorityCol]; log(caseID, "casePriority: " + casePriority.innerText); }
 
-        if (statusField.innerText === "Customer Response Provided") {
-            statusField.style.background = "red";
-            statusField.style.color = "white";
+        // initialise total cases variables
+        let totalCases = 0;
+        let totalCRP = 0;
+
+        // Loop over desired Column(s) innerHTML and apply styling
+        if (statusField.innerText === "Customer Response Provided" || casePriority === "P1 - Emergency") {
+            caseRows[row].style.background = "lightyellow";
+            //caseRows[row].style.color = "white";
 
             totalCRP += 1;
             log("Highlighted case " + caseID);
+        }
+        if (casePriority.innerText == "P3 - Normal" || casePriority.innerText == "P4 - Low" || casePriority.innerText == "RFE - Request for Enhancement") {
+            caseRows[row].style.background = "#b3ffff";
         }
 
         totalCases += 1;
@@ -59,4 +76,11 @@ function getCaseStatus() {
     log("Total Custom Responded cases: " + totalCRP);
 }
 
+const DEBUG = true; // turn debug logging on
+const WAITTIME = 5000; // we wait to run this script every 5 seconds due to page refreshes
+
+// Wait until last table element has loaded before running the function, otherwise it kinda breaks
 waitForKeyElements("div.x-grid3-row.x-grid3-row-last", getCaseStatus);
+
+// Run the script every WAITTIME amount to combat page refreshes
+setTimeout(getCaseStatus, WAITTIME);
